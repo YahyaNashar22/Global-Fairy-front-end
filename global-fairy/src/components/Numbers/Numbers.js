234844755737096ebd5c9c22db269e9322b99a34
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-27 22:18:58 +0300

    images

--- global-fairy/src/components/Numbers/Numbers.js
+++ global-fairy/src/components/Numbers/Numbers.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import style from "./Numbers.module.css"
-import shoes from "../../assets/images/shoes.webp"
-import fashion1 from "../../assets/images/fashion1.jpg"
-import makeup3 from "../../assets/images/makeup3.jpg"
-import fashion7 from "../../assets/images/fashion7.jpg"
-import fashion4 from "../../assets/images/fashion4.jpg"
+// import shoes from "../../assets/images/shoes.webp"
+// import fashion1 from "../../assets/images/fashion1.jpg"
+// import makeup3 from "../../assets/images/makeup3.jpg"
+// import fashion7 from "../../assets/images/fashion7.jpg"
+// import fashion4 from "../../assets/images/fashion4.jpg"
 
 function Numbers() {
   return (
@@ -12,13 +12,18 @@ function Numbers() {
       <h1 style={style.h1}>Numbers Behind Our Success</h1>
       <section className={style.imageSection}>
       <section className={style.imageSection1}>
+        <figure className={style.imageA}>1000+ High-Quality Products</figure>
+        <figure className={style.imageB}> Happy Customers</figure>
+        <figure className={style.imageC}>15+ International Brands</figure>
+{/* 
         <figure> <img className={style.imageA} src={shoes} alt="showcasea hight quality product" ></img> </figure>
         <figure> <img src={fashion1} className={style.imageB} alt="showcas a happy customer" ></img>  </figure>
-        <figure><img src={makeup3} className={style.imageC} alt="showcae some products of some brands" ></img> </figure>
+        <figure><img src={makeup3} className={style.imageC} alt="showcae some products of some brands" ></img> </figure> */}
       </section>
       <section className={style.imageSection2}>
-        <figure><img src={fashion7} className={style.imageD} alt="showcse different colors of a product" ></img> </figure>
-        <figure><img src={fashion4} className={style.imageE} alt="showase a fashionable outfit" ></img></figure>
+      <figure className={style.imageD} >Options For all Tastes</figure>
+        <figure className={style.imageE}>7+ Years in Business </figure>        {/* <figure><img src={fashion7} className={style.imageD} alt="showcse different colors of a product" ></img> </figure> */}
+        {/* <figure><img src={fashion4} className={style.imageE} alt="showase a fashionable outfit" ></img></figure> */}
       </section>
       </section>
     </container>
--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,11 +1,15 @@
 // import Header from "../../layout/Header/Header"
 import "./Home.module.css"
 import Numbers from "../../components/Numbers/Numbers"
+import BrandHeader from "../../components/brandHeader/BrandHeader"
+import CategoriesHeader from "../../components/Categories/CategoriesHeader"
 
 const Home = () => {
 return(
     <div>
         {/* <Header /> */}
+        {/* <CategoriesHeader></CategoriesHeader>
+        <BrandHeader></BrandHeader> */}
         <Numbers/>
 
 
