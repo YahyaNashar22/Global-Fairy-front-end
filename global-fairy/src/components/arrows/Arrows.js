70bbfa76cfd07dca1a05d6d76162c1778811e912
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-28 22:50:15 +0300

    finishing arrow section

--- global-fairy/src/components/arrows/Arrows.js
+++ global-fairy/src/components/arrows/Arrows.js
@@ -4,14 +4,13 @@ import back from "../../assets/icons/back.png"
 import next from "../../assets/icons/next.png"
 
 export default function Arrows() {
-  return (
-    <div className={style.arrowsContainer}>
-<span className={`${style.nbSpan} ${style.back}`}><img src={back} alt="back arrow" className={style.imgSpan}/></span>
-<span className={`${style.nbSpan} ${style.nbs}`}>1</span>
-<span className={`${style.nbSpan} ${style.nbs}`}>2</span>
-<span className={`${style.nbSpan} ${style.nbs}`}>3</span>
-<span className={`${style.nbSpan} ${style.nextA}`}><img src={next} alt="next arrow"  className={style.imgSpan}/></span>
-
-    </div>
-  )
+    return (
+        <div className={style.arrowsContainer}>
+            <span className={`${style.nbSpan} ${style.back}`}><img src={back} alt="back arrow" className={style.imgSpan} /></span>
+            <span className={`${style.nbSpan} ${style.nbs}`}>1</span>
+            <span className={`${style.nbSpan} ${style.nbs}`}>2</span>
+            <span className={`${style.nbSpan} ${style.nbs}`}>3</span>
+            <span className={`${style.nbSpan} ${style.nextA}`}><img src={next} alt="next arrow" className={style.imgSpan} /></span>
+        </div>
+    )
 }
