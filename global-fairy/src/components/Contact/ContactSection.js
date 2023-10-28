46a098fa1fef746e4c8e472f8b99ccf5ad754007
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-28 15:30:47 +0300

    HTML/CSS Done

--- global-fairy/src/components/Contact/ContactSection.js
+++ global-fairy/src/components/Contact/ContactSection.js
@@ -1,45 +1,41 @@
 import React from 'react'
 import styles from './ContactSection.module.css'
+import callIcon from "../../assets/icons/telephone.png"
+import emailIcon from "../../assets/icons/mail.png"
 export default function ContactSection() {
     return (
         <container className={styles.contactContainer}>
 
             <section className={styles.contactNb}>
                 <aricle className={styles.callUs}>
-                    <span>
-                        <h4>Call To Us</h4>
-                    </span>
-                    <span>
-
-                    <p>We are available 7 days a week.</p>
-                        <p>Phone: +961000000</p>
-                    </span>
-
+                    <div className={styles.contactTitles}>
+                    <span className={styles.imgHolder}> <img src={callIcon} alt="Call Icon" /> </span> <h4>Call To Us</h4>
+                    </div>
+                    <p className={styles.textHolder}> <span className={styles.imgHolderX}> <img src={callIcon} alt="Call Icon" /> </span>We are available 7 days a week.</p>
+                    <p className={styles.textHolder}>Phone: +961000000</p>
                 </aricle>
                 <aricle className={styles.msgUs}>
-                <h4>Write To Us</h4>
-
-                <p>Fill out our form and we will contact you within 24 hours.</p>
-                    <p>Emails: customer@exclusive.com</p>
-                    <p>Emails: support@exclusive.com</p>
-                    </aricle>
-                    </section>
-            <section className={styles.contactForm}>
-                <span>
-                    <input className={styles.inputs} placeholder="Your Name"></input>
-                    <input className={styles.inputs} placeholder="Your Email"></input>
-                    <input className={styles.inputs} placeholder="Your Phone"></input>
-
-                </span>
-<br></br>
-                <span>
-                <input className={styles.msgInput} placeholder="Your Phone"></input>
-
-                </span>
-                <br></br>
-                <button className={styles.msgBtn}>Send Message</button>
-
+                    <div className={styles.contactTitles}><span className={styles.imgHolder}> <img src={emailIcon} alt="Message Icon" /> </span> <h4>Write To Us</h4></div>
+                    <p className={styles.textHolder}> <span className={styles.imgHolderX}> <img src={emailIcon} alt="Message Icon" /> </span>Fill out our form and we will contact you within 24 hours.</p>
+                    <p className={styles.textHolder}>Emails: customer@exclusive.com</p>
+                    <p className={styles.textHolder}>Emails: support@exclusive.com</p>
+                </aricle>
             </section>
+            <form className={styles.contactForm}>
+                <div className={styles.inputsHolder}>
+                    <input className={styles.inputs} type="text" id="name" name="name" placeholder="Your Name" required></input>
+                    <input className={styles.inputs} type="email" id="email" name="email" placeholder="Your Email" required></input>
+                    <input className={styles.inputs} type="tel" id="phone" name="phone" pattern="^\d{8}$" placeholder="Your Phone" required></input>
+                </div>
+                <div className={styles.msgHolder}>
+                    <textarea className={styles.msgInput} type="textarea" placeholder="Your Message"></textarea>
+
+                </div>
+                <div className={styles.btnHolder}>
+                    <button className={styles.msgBtn} type='submit' value='submit' >Send Message</button>
+                </div>
+
+            </form>
         </container>
     )
 }
