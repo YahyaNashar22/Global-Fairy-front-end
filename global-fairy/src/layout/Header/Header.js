8c2355ef98a961a14dd0b174760a575edc88a8d0
Rachwan <rachwan.harb2023@gmail.com>
2023-10-29 08:42:50 +0200

    Done HTML and CSS for the header

--- global-fairy/src/layout/Header/Header.js
+++ global-fairy/src/layout/Header/Header.js
@@ -1,13 +1,35 @@
 import React from "react";
-import Search from '../../components/Search/Search'
-import './Header.module.css';
+import style from './Header.module.css';
+import logo from '../../assets/images/logo.jpg';
+import userIcon from '../../assets/images/user.png';
+import search from '../../assets/images/search.svg';
 
 const Header = () => {
     return(
-        <div>
-            <h1>Hello To Global Fairy Website</h1>
-            <Search />
-        </div>
+        <header className={style.header}>
+            <div className={style.container}>
+                <a href="/home" className={style.logo}>
+                    <img src={logo} alt="Logo" />
+                </a>
+                <div className={style.content}>
+                    <nav className={style.nav}>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                            <li><a href="/contact">Contact</a></li>
+                            <li><a href="/signup">Sign Up</a></li>
+                        </ul>
+                    </nav>
+                    <form action="/search" method="get" className={style.search}>
+                        <input type="text" name="Search" placeholder="Search for products..." />
+                        <img src={search} alt="" />
+                    </form>
+                    <div className={style.userIcon}>
+                    <img src={userIcon} alt="User Icon" />
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
