bd3a8c85b41d2f3dec873462f285c5394e54f1e6
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-29 23:22:10 +0200

    delete comments

--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,19 +1,11 @@
-// import Header from "../../layout/Header/Header"
+import Header from "../../layout/Header/Header"
 import "./Home.module.css"
-// import Numbers from "../../components/Numbers/Numbers"
-// import BrandHeader from "../../components/brandHeader/BrandHeader"
-// import CategoriesHeader from "../../components/Categories/CategoriesHeader"
-import ContactSection from "../../components/Contact/ContactSection"
+
 
 const Home = () => {
 return(
     <div>
-        {/* <Header /> */}
-        {/* <CategoriesHeader></CategoriesHeader>
-        <BrandHeader></BrandHeader> */}
-        {/* <Numbers/> */}
-        <ContactSection></ContactSection>
-
+        <Header />
 
     </div>
 )
