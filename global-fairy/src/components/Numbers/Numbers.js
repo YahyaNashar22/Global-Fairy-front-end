cf7b2da92eef90a71550ab37684096aa8a308f98
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-29 23:27:26 +0200

    remove comments

--- global-fairy/src/components/Numbers/Numbers.js
+++ global-fairy/src/components/Numbers/Numbers.js
@@ -10,16 +10,10 @@ function Numbers() {
           <figcaption className={style.imageA}>1000+<br /> High-Quality Products</figcaption>
           <figcaption className={style.imageB}> 200+<br /> Happy Customers</figcaption>
           <figcaption className={style.imageC}>15+ <br />International Brands</figcaption>
-          
-        {/* <figcaption className={style.imageA}> <img  src={shoes} alt="showcasea hight quality product" ></img> </figcaption>
-        <figcaption className={style.imageB}> <img src={fashion1}  alt="showcas a happy customer" ></img>  </figcaption>
-        <figcaption  className={style.imageC}><img src={makeup3} alt="showcae some products of some brands" ></img> </figcaption>  */}
         </section>
         <section className={style.imageSection2}>
           <figcaption className={style.imageD} >Options For all Tastes</figcaption>
           <figcaption className={style.imageE}>7+ <br />Years in Business </figcaption>
-          {/* <figure><img src={fashion7} className={style.imageD} alt="showcse different colors of a product" ></img> </figure> */}
-          {/* <figure><img src={fashion4} className={style.imageE} alt="showase a fashionable outfit" ></img></figure> */}
         </section>
       </section>
     </main>
--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,16 +1,12 @@
 // import Header from "../../layout/Header/Header"
 import "./Home.module.css"
 import Numbers from "../../components/Numbers/Numbers"
-import BrandHeader from "../../components/brandHeader/BrandHeader"
-import CategoriesHeader from "../../components/Categories/CategoriesHeader"
-import Nb from "../../components/nbTest/Nb"
+
 
 const Home = () => {
 return(
     <div>
         {/* <Header /> */}
-        {/* <CategoriesHeader></CategoriesHeader>
-        <BrandHeader></BrandHeader> */}
         <Numbers/>
 
 
