3184807c00cc2d451053421d52639a98a1fbb914
Rachwan <rachwan.harb2023@gmail.com>
2023-10-29 22:00:03 +0200

    Create  the folders

--- global-fairy/src/layout/Header/Header.js
+++ global-fairy/src/layout/Header/Header.js
@@ -5,7 +5,7 @@ import './Header.module.css';
 const Header = () => {
     return(
         <div>
-            <h1>Hello To Global Fairy Website</h1>
+            
             <Search />
         </div>
     )
--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import Header from "../../layout/Header/Header"
+import Hero from "../../layout/Hero/Hero"
 import "./Home.module.css"
 
 const Home = () => {
 return(
     <div>
         <Header />
+        <Hero />
     </div>
-)
+    )
 }
 
 export default Home;
