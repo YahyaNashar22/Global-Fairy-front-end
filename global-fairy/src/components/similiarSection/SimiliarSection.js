0505c14f04bbf0a48456a9b0455d1c57f1a07ed1
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-29 10:59:42 +0200

    similar product responsive

--- global-fairy/src/components/similiarSection/SimiliarSection.js
+++ global-fairy/src/components/similiarSection/SimiliarSection.js
@@ -5,12 +5,13 @@ import SimiliarProduct from '../similiarProducts/SimiliarProduct'
 export default function SimiliarSection() {
   return (
     <main className={style.main}>
-    <h1 style={style.h1}>You might also like</h1>
-<section className={style.productsHolder}>
-      <SimiliarProduct></SimiliarProduct>
-      <SimiliarProduct></SimiliarProduct>
-      <SimiliarProduct></SimiliarProduct>
-      <SimiliarProduct></SimiliarProduct>
+      <h1 style={style.h1}>You might also like</h1>
+      <section className={style.productsHolder}>
+        <SimiliarProduct></SimiliarProduct>
+        <SimiliarProduct></SimiliarProduct>
+        <SimiliarProduct></SimiliarProduct>
+        <SimiliarProduct></SimiliarProduct>
+        <SimiliarProduct></SimiliarProduct>
       </section>
     </main>
   )
