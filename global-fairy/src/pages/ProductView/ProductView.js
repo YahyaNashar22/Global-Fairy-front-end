78c776f4bd5e06c6b7fcf5927b3cb862dc2bef60
Rachwan <rachwan.harb2023@gmail.com>
2023-10-31 01:26:50 +0200

    Created the folder page and the file intialization

---
+++ global-fairy/src/pages/ProductView/ProductView.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './ProductView.module.css'
+
+const ProductView = () => {
+return(
+    <div>
+        
+    </div>
+)
+}
+
+export default ProductView;
