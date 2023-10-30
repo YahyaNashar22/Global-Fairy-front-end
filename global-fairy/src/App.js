1830dd7b453dd96eeec82ef60a3b4336460ed961
whathow <marwa@gmail.com>
2023-10-30 21:18:44 +0200

    about

--- global-fairy/src/App.js
+++ global-fairy/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Addproduct from './components/Addproduct/Addproduct';
 import Home from "./pages/Home/Home"
+import About from './pages/About/About';
+
 
 function App() {
   return (
     <div className="app">
       <Home />
+      <About/>
     </div>
   );
 }
