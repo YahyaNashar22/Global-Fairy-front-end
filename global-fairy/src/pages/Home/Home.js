272bf0d81df0aa1fee648022ad76222b035083b8
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-30 10:59:51 +0200

    Deleted the Contact and Numbers folders

--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,19 +1,10 @@
-// import Header from "../../layout/Header/Header"
+import Header from "../../layout/Header/Header"
 import "./Home.module.css"
-// import Numbers from "../../components/Numbers/Numbers"
-// import BrandHeader from "../../components/brandHeader/BrandHeader"
-import Arrows from "../../components/arrows/Arrows";
+
 const Home = () => {
 return(
     <div>
-        {/* <Header /> */}
-        {/* <CategoriesHeader></CategoriesHeader>
-        <BrandHeader></BrandHeader> */}
-        {/* <Numbers/> */}
-        {/* <ContactSection></ContactSection> */}
-        {/* <ProductNbs></ProductNbs> */}
-<Arrows></Arrows>
-
+        <Header />
     </div>
 )
 }
