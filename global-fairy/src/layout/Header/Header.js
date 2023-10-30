5afd947fe9649c0dee26ebb519444d5af8456628
Rachwan <rachwan.harb2023@gmail.com>
2023-10-30 20:38:04 +0200

    Make the header responsive

--- global-fairy/src/layout/Header/Header.js
+++ global-fairy/src/layout/Header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import style from './Header.module.css';
 import logo from '../../assets/images/logo.jpg';
 import userIcon from '../../assets/images/user.png';
@@ -6,17 +6,50 @@ import NavBar from "../NavBar/NavBar";
 import Search from '../../components/Search/Search'
 
 const Header = () => {
-    return(
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    const toggleNav = () => {
+        setIsNavOpen(!isNavOpen);
+    }
+
+    // Use useEffect to ensure the navigation menu is shown when screen width > 992px
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 992) {
+                setIsNavOpen(true);
+            } else {
+                setIsNavOpen(false);
+            }
+        };
+
+        // Add event listener to handle window resize
+        window.addEventListener('resize', handleResize);
+
+        // Call the handleResize function on initial load
+        handleResize();
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    return (
         <header className={style.header}>
             <div className={style.container}>
                 <a href="/home" className={style.logo}>
                     <img src={logo} alt="Logo" />
                 </a>
                 <div className={style.content}>
-                    <NavBar />
+                    {isNavOpen && <NavBar />}
                     <Search />
                     <div className={style.userIcon}>
-                    <img src={userIcon} alt="User Icon" />
+                        <img src={userIcon} alt="User Icon" />
+                    </div>
+                    <div className={style.burger} onClick={toggleNav}>
+                        <div className={`${style.bar} ${isNavOpen ? style.open : ''}`}></div>
+                        <div className={`${style.bar} ${isNavOpen ? style.open : ''}`}></div>
+                        <div className={`${style.bar} ${isNavOpen ? style.open : ''}`}></div>
                     </div>
                 </div>
             </div>
--- global-fairy/src/layout/NavBar/NavBar.js
+++ global-fairy/src/layout/NavBar/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import style from './NavBar.module.css';
 
-const NavBar = () => {
-  return (
-    <nav className={style.nav}>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/signup">Sign Up</a></li>
-    </ul>
-    </nav>
-  )
+const NavBar = ({ toggleNav }) => {
+    return (
+        <nav className={style.nav}>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about">About</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/signup">Sign Up</a></li>
+            </ul>
+        </nav>
+    )
 }
+
 export default NavBar;
