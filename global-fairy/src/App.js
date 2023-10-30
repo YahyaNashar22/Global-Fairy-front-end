078f0a9ad8611d084b4665437774b513f455bb8e
whathow <marwa@gmail.com>
2023-10-30 22:35:47 +0200

    edditabout

--- global-fairy/src/App.js
+++ global-fairy/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
-import Addproduct from './components/Addproduct/Addproduct';
+// import Addproduct from './components/Addproduct/Addproduct';
 import Home from "./pages/Home/Home"
-import About from './pages/About/About';
 
 
 function App() {
   return (
     <div className="app">
       <Home />
-      <About/>
     </div>
   );
 }
