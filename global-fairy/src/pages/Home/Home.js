d39658e78a4734f40a7938d8c90311a889741bb7
Rayan ALi <rayan.ali.8205@gmail.com>
2023-10-30 10:42:37 +0200

    Delete the Numbers folder

--- global-fairy/src/pages/Home/Home.js
+++ global-fairy/src/pages/Home/Home.js
@@ -1,12 +1,12 @@
 import Header from "../../layout/Header/Header"
 import "./Home.module.css"
-
+import ContactSection from "../../components/Contact/ContactSection"
 
 const Home = () => {
 return(
     <div>
         <Header />
-
+<ContactSection></ContactSection>
     </div>
 )
 }
