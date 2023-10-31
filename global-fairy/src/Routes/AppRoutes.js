3cc5bd2009d59531b6a965cc473f1a336ed07e5a
Rachwan <rachwan.harb2023@gmail.com>
2023-10-31 20:51:37 +0200

    Delete the AddProuct folder + Edit the Routes folder

--- global-fairy/src/Routes/AppRoutes.js
+++ global-fairy/src/Routes/AppRoutes.js
@@ -1,29 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import '../App.css'
-import Home from '../pages/Home/Home'
-import About from '../pages/About/About'
-import Contact from '../pages/ContactUs/Contact'
-import Brand from '../pages/Brand/Brand'
-import Category from '../pages/Category/Category'
-import Dashboard from '../pages/Dashboard/Dashboard'
-import AddProduct from '../pages/AddProduct/AddProduct'
+import Home from '../pages/Home/Home';
+import About from '../pages/About/About';
+import Contact from '../pages/ContactUs/Contact';
+import Brand from '../pages/Brand/Brand';
+import Category from '../pages/Category/Category';
+import Dashboard from '../pages/Dashboard/Dashboard';
+import Addproduct from '../pages/Addproduct/Addproduct';
+import LayoutWithHeaderFooter from './LayoutWithHeaderFooter';
+import LayoutWithoutHeaderFooter from './LayoutWithoutHeaderFooter';
 
 function AppRoutes() {
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/brand" element={<Brand />} />
-          <Route path="/category" element={<Category />} />
-          <Route path="/dashborad" element={<Dashboard />} />
-          <Route path="/addproduct" element={<AddProduct />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        {/* Routes with Header and Footer */}
+        <Route index element={<LayoutWithHeaderFooter> <Home /> </LayoutWithHeaderFooter>} />
+        <Route path="/home" element={<LayoutWithHeaderFooter> <Home /> </LayoutWithHeaderFooter>} />
+        <Route path="/about" element={<LayoutWithHeaderFooter> <About /> </LayoutWithHeaderFooter>} />
+        <Route path="/contact" element={<LayoutWithHeaderFooter> <Contact /> </LayoutWithHeaderFooter>} />
+        <Route path="/brand" element={<LayoutWithHeaderFooter> <Brand /> </LayoutWithHeaderFooter>} />
+        <Route path="/category" element={<LayoutWithHeaderFooter> <Category /> </LayoutWithHeaderFooter>} />
+
+        {/* Routes without Header and Footer */}
+        <Route path="/dashboard" element={<LayoutWithoutHeaderFooter> <Dashboard /> </LayoutWithoutHeaderFooter>} />
+        <Route path="/addproduct" element={<LayoutWithoutHeaderFooter> <Addproduct /> </LayoutWithoutHeaderFooter>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
