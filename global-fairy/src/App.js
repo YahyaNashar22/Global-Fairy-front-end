d269f0fb5b9edcfa78c26231b5e398e9db5767fa
whathow <marwa@gmail.com>
2023-10-31 15:59:18 +0200

    Addproduct form page

--- global-fairy/src/App.js
+++ global-fairy/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import Home from "./pages/Home/Home"
+import Addproduct from './pages/Addproduct/Addproduct';
+// import Home from "./pages/Home/Home"
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Addproduct/>
     </div>
   );
 }
