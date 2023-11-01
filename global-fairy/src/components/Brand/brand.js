5f05dc134810dea172da3e96870876e3010067b7
Rayan ALi <rayan.ali.8205@gmail.com>
2023-11-01 23:32:24 +0200

    add links

--- global-fairy/src/components/Brand/brand.js
+++ global-fairy/src/components/Brand/brand.js
@@ -10,7 +10,7 @@ import zara from '../../assets/images/zara.jpg';
 import nyx from '../../assets/images/nyx.jpg';
 import nike from '../../assets/images/nike.jpg';
 import nars from '../../assets/images/nars.jpg';
-
+import { Link } from "react-router-dom"
 const Brand = () => {
     return (
         <section className={style.brandSection}>
@@ -18,36 +18,58 @@ const Brand = () => {
             <div className={style.imageContainer}>
                 <div className={style.row}>
                     <div className={style.twoBrands}>
-                        <img src={shein} alt="Image 1"/>
-                        <img src={addidas} alt="Image 2g"/>
+                        {/* <Link to="Products/Brand/Shein"> */}
+                        <img src={shein} className={style.brandImage} alt="Image 1" />
+                        {/* </Link> */}
+                        {/* <Link to="Products/Brand/Adidas"> */}
+                        <img src={addidas} className={style.brandImage} alt="Image 2g" />
+                        {/* </Link> */}
                     </div>
-                <div className={style.row}>
-                    <div className={style.twoBrands}>
-                        <img src={handm} alt="Image 3"/>
-                        <img src={hudaBeauty} alt="Image 4"/>
-                    </div>
-                </div>
-                </div>
-                <div className={style.row}>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                        <img src={mango} className={style.deleteImage} alt="Image 5"/>
-                        <img src={bershka} alt="Image 6"/>
-                        <img src={robyRose} alt="Image 7"/>
+                            {/* <Link to="Products/Brand/HM"> */}
+                            <img src={handm} className={style.brandImage} alt="Image 3" />
+                            {/* </Link> */}
+                            {/* <Link to="Products/Brand/HudaBeauty"> */}
+                            <img src={hudaBeauty} className={style.brandImage} alt="Image 4" />
+                            {/* </Link> */}
                         </div>
                     </div>
                 </div>
                 <div className={style.row}>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            <img src={zara} alt="Image 8"/>
-                            <img src={nyx}alt="Image 9"/>
+                            {/* <Link to="Products/Brand/Mango"> */}
+                            <img src={mango} className={`${style.deleteImage} ${style.brandImage} `} alt="Image 5" />
+                            {/* </Link> */}
+                            {/* <Link to="Products/Brand/Bershka"> */}
+                            <img src={bershka} className={style.brandImage} alt="Image 6" />
+                            {/* </Link> */}
+                            {/* <Link to="Products/Brand/RobyRose"> */}
+                            <img src={robyRose} className={style.brandImage} alt="Image 7" />
+                            {/* </Link> */}
+                        </div>
+                    </div>
+                </div>
+                <div className={style.row}>
+                    <div className={style.row}>
+                        <div className={style.twoBrands}>
+                            {/* <Link to="Products/Brand/Zara"> */}
+                            <img src={zara} className={style.brandImage} alt="Image 8" />
+                            {/* </Link> */}
+                            {/* <Link to="Products/Brand/Nyx"> */}
+                            <img src={nyx} className={style.brandImage} alt="Image 9" />
+                            {/* </Link> */}
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            <img src={nike} alt="Image 10"/>
-                            <img src={nars}alt="Image 11"/>
+                            {/* <Link to="Products/Brand/Nike"> */}
+                            <img src={nike} className={style.brandImage} alt="Image 10" />
+                            {/* </Link> */}
+                            {/* <Link to="Products/Brand/Nars"> */}
+                            <img src={nars} className={style.brandImage} alt="Image 11" />
+                            {/* </Link> */}
                         </div>
                     </div>
                 </div>
