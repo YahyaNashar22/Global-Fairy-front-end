b8860ee68d529b63616db3db70e21516174ab652
Rayan ALi <rayan.ali.8205@gmail.com>
2023-11-02 22:12:40 +0200

    add brand Pages Link and some css edits

--- global-fairy/src/components/Brand/brand.js
+++ global-fairy/src/components/Brand/brand.js
@@ -12,64 +12,70 @@ import nike from '../../assets/images/nike.jpg';
 import nars from '../../assets/images/nars.jpg';
 import { Link } from "react-router-dom"
 const Brand = () => {
+    // const scrollToTop=()=>{
+    //     setTimeout(()=>{
+    //         window.scrollTo(0,0)
+    //     },1)
+       
+    // }
     return (
         <section className={style.brandSection}>
             <h1 className={style.heading}>Shop by Brand </h1>
             <div className={style.imageContainer}>
                 <div className={style.row}>
                     <div className={style.twoBrands}>
-                        {/* <Link to="Products/Brand/Shein"> */}
-                        <img src={shein} className={style.brandImage} alt="Image 1" />
-                        {/* </Link> */}
-                        {/* <Link to="Products/Brand/Adidas"> */}
-                        <img src={addidas} className={style.brandImage} alt="Image 2g" />
-                        {/* </Link> */}
+                        <Link to="Products/Brand/Shein"  style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink} >
+                        <img src={shein} className={style.brandImage} alt="Shein Logo" />
+                        </Link>
+                        <Link to="Products/Brand/Adidas"  style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                        <img src={addidas} className={style.brandImage} alt="Adidas Logo" />
+                        </Link>
                     </div>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            {/* <Link to="Products/Brand/HM"> */}
-                            <img src={handm} className={style.brandImage} alt="Image 3" />
-                            {/* </Link> */}
-                            {/* <Link to="Products/Brand/HudaBeauty"> */}
-                            <img src={hudaBeauty} className={style.brandImage} alt="Image 4" />
-                            {/* </Link> */}
+                            <Link to="Products/Brand/HM"  style={{'textDecoration':'none', "color":'inherit'}}className={style.brandImageLink}>
+                            <img src={handm}   className={style.brandImage} alt="H and M Logo" />
+                            </Link>
+                            <Link to="Products/Brand/HudaBeauty"  style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={hudaBeauty} className={style.brandImage} alt="HudaBeauty Logo" />
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className={style.row}>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            {/* <Link to="Products/Brand/Mango"> */}
-                            <img src={mango} className={`${style.deleteImage} ${style.brandImage} `} alt="Image 5" />
-                            {/* </Link> */}
-                            {/* <Link to="Products/Brand/Bershka"> */}
-                            <img src={bershka} className={style.brandImage} alt="Image 6" />
-                            {/* </Link> */}
-                            {/* <Link to="Products/Brand/RobyRose"> */}
-                            <img src={robyRose} className={style.brandImage} alt="Image 7" />
-                            {/* </Link> */}
+                            <Link to="Products/Brand/Mango" style={{'textDecoration':'none', "color":'inherit'}} className={`${style.brandImageLink} ${style.deleteImage}`}>
+                            <img src={mango} className={`${style.deleteImage} ${style.brandImage} `} alt="Mango Logo" />
+                            </Link>
+                            <Link to="Products/Brand/Bershka"  style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={bershka} className={style.brandImage} alt="Bershka Logo" />
+                            </Link>
+                            <Link to="Products/Brand/RobyRose"  style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={robyRose} className={style.brandImage} alt="RobyRose Logo" />
+                            </Link>
                         </div>
                     </div>
                 </div>
                 <div className={style.row}>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            {/* <Link to="Products/Brand/Zara"> */}
-                            <img src={zara} className={style.brandImage} alt="Image 8" />
-                            {/* </Link> */}
-                            {/* <Link to="Products/Brand/Nyx"> */}
-                            <img src={nyx} className={style.brandImage} alt="Image 9" />
-                            {/* </Link> */}
+                            <Link to="Products/Brand/Zara"    style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={zara} className={style.brandImage} alt="Zara Logo" />
+                            </Link>
+                            <Link to="Products/Brand/Nyx"     style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={nyx} className={style.brandImage} alt="Nyx Logo" />
+                            </Link>
                         </div>
                     </div>
                     <div className={style.row}>
                         <div className={style.twoBrands}>
-                            {/* <Link to="Products/Brand/Nike"> */}
-                            <img src={nike} className={style.brandImage} alt="Image 10" />
-                            {/* </Link> */}
-                            {/* <Link to="Products/Brand/Nars"> */}
-                            <img src={nars} className={style.brandImage} alt="Image 11" />
-                            {/* </Link> */}
+                            <Link to="Products/Brand/Nike"    style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={nike} className={style.brandImage} alt="Nike logo" />
+                            </Link>
+                            <Link to="Products/Brand/Nars" style={{'textDecoration':'none', "color":'inherit'}} className={style.brandImageLink}>
+                            <img src={nars} className={style.brandImage} alt="Nars Logo" />
+                            </Link>
                         </div>
                     </div>
                 </div>
