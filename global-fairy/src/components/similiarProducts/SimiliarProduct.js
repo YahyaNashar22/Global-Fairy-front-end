eb1f283176077ad36446a750eb074bd0151eb10b
Rachwan <rachwan.harb2023@gmail.com>
2023-11-08 15:23:24 +0200

    Done with the fetching

--- global-fairy/src/components/similiarProducts/SimiliarProduct.js
+++ global-fairy/src/components/similiarProducts/SimiliarProduct.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import style from "./Similiar.module.css"
 import productImg from "../../assets/images/product.png"
+import axios from 'axios'
 
-export default function SimiliarProduct() {
+export default function SimiliarProduct({productDetails}) {
+  const product=productDetails
+  console.log(product.name)
   return (
     <div className={style.card}>
       <div className={style.imageSec}>
-        <img src={productImg} alt="Product" />
+        {/* <img src={productImg} alt="Product" /> */}
+        <img src={`http://localhost:5000/${productDetails.images[0]}`} alt="Product" />
+
       </div>
       <div className={style.details}>
-        <span className={style.title}>Striped Flutter Sleeve Overlap Collar Peplum Hem Blouse</span>
-        <span className={style.priceCard}>$78.5</span>
+        <span className={style.title}>{productDetails.name}</span>
+        <span className={style.priceCard}>${productDetails.price}</span>
       </div>
     </div>
   )
