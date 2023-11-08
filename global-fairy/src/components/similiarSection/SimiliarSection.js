612c5fe8d91ce7c77d7c7229aef0a9f779cdc0a2
Rachwan <rachwan.harb2023@gmail.com>
2023-11-08 12:50:47 +0200

    Passed the category id from ProductView into SimilarSection

--- global-fairy/src/components/similiarSection/SimiliarSection.js
+++ global-fairy/src/components/similiarSection/SimiliarSection.js
@@ -2,10 +2,10 @@ import React from 'react'
 import style from "./SimiliarSection.module.css"
 import SimiliarProduct from '../similiarProducts/SimiliarProduct'
 
-export default function SimiliarSection() {
+export default function SimiliarSection({ similiarId }) {
   return (
     <section className={style.main}>
-      <h1 className={style.title}>You might also like</h1>
+      <h1 className={style.title}>You might also like - THE ID: {similiarId}</h1>
       <div className={style.productsHolder}>
         <SimiliarProduct></SimiliarProduct>
         <SimiliarProduct></SimiliarProduct>
