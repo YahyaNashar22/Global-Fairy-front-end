e9418961a2f0eaccc37e3cb32efeaecf576ae359
YahyaNashar22 <cenations.people@live.com>
2024-01-04 13:39:18 +0200

    fixed only logged in users can buy

--- global-fairy/src/components/PurshaseButtons/PurshaseButtons.js
+++ global-fairy/src/components/PurshaseButtons/PurshaseButtons.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import style from "./PurshaseButtons.module.css";
 import axios from "axios";
+import { UserContext } from "../../context/UserContext.js";
 import { useShoppingContext } from "../../context/ShoppingContext";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const PurchaseButtons = ({ index, stock, productId, fetchProductData }) => {
   // This for the count button
   const [count, setCount] = useState(1);
-  const [addedToCart, setAddedToCart] = useState(false); 
-  const {inventory, addToCart} = useShoppingContext(); 
+  const [addedToCart, setAddedToCart] = useState(false);
+  const { inventory, addToCart } = useShoppingContext();
+  const { user } = useContext(UserContext);
+  const notify = () => toast("Only logged in users can buy !");
+  console.log(user);
 
   const decreaseOne = () => {
     if (count > 1) {
@@ -21,41 +27,56 @@ const PurchaseButtons = ({ index, stock, productId, fetchProductData }) => {
     }
   };
 
-  const updateStock = async()=>{
-    const product = await fetchProductData();
-    console.log("my productt: ", product)
-    addToCart(product)
-        console.log(inventory)
+  const updateStock = async () => {
+    if (!user.id) {
+      notify();
+    } else {
+      const product = await fetchProductData();
+      console.log("my productt: ", product);
+      addToCart(product);
+      console.log(inventory);
+    }
     // try{
     //     const response=await axios.put(`${process.env.REACT_APP_PATH}/product/updateStock/${productId}/${index}/${count}`)
     //   if(response){
     //     fetchProductData();
     //     setAddedToCart(true);
     //     setCount(1);
-        
+
     //   }
     // }
     // catch(error){
     //     console.log(error.message)
     // }
-
-} 
+  };
 
   return (
     <>
-      <div className={style.purshaseButtons} style={{ display: stock !== 0 ? "flex" : "none" }}>
+      <div
+        className={style.purshaseButtons}
+        style={{ display: stock !== 0 ? "flex" : "none" }}
+      >
         <button className={style.counter}>
-          <span className={style.decrease} onClick={decreaseOne}>-</span>
+          <span className={style.decrease} onClick={decreaseOne}>
+            -
+          </span>
           <span className={style.count}>{count}</span>
-          <span className={style.increase} onClick={increaseOne}>+</span>
+          <span className={style.increase} onClick={increaseOne}>
+            +
+          </span>
         </button>
-        <button 
-        className={style.addToCard}
-        style={{ backgroundColor: addedToCart ? "#bd5580" : "#D96093" }}
-        onClick={()=>updateStock()}
-        >{addedToCart ? "Added to Cart!" : "Add to Cart"}</button>
+        <button
+          className={style.addToCard}
+          style={{ backgroundColor: addedToCart ? "#bd5580" : "#D96093" }}
+          onClick={() => updateStock()}
+        >
+          {addedToCart ? "Added to Cart!" : "Add to Cart"}
+        </button>
       </div>
-      <button className={style.soldOut} style={{ display: stock === 0 ? "block" : "none" }}>
+      <button
+        className={style.soldOut}
+        style={{ display: stock === 0 ? "block" : "none" }}
+      >
         Sold Out
       </button>
     </>
